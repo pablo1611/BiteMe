15902a17f5773790a4e55d6ff9cdc63c59f4af9c
Pablo Dobrzynski <pablo161198@gmail.com>
2024-07-23 17:36:00 +0300

    3rd commit of start

---
+++ clientSide/src/entities/BranchManager.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class BranchManager {
+    private String branchName;
+
+    // Getters and Setters
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+}
+
---
+++ clientSide/src/entities/CEO.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class CEO {
+    private boolean isCEO;
+
+    // Getters and Setters
+    public boolean isCEO() {
+        return isCEO;
+    }
+
+    public void setCEO(boolean isCEO) {
+        this.isCEO = isCEO;
+    }
+}
