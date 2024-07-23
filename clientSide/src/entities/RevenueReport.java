5e8de8c69cdf18039bdd6eed6dfdf3757c66e1ec
Pablo Dobrzynski <pablo161198@gmail.com>
2024-07-23 16:58:17 +0300

    initial version of client side 2

---
+++ clientSide/src/entities/RevenueReport.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class RevenueReport extends Report {
+    private float totalRevenue;
+
+    // Getters and Setters
+    public float getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    public void setTotalRevenue(float totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+}
+
