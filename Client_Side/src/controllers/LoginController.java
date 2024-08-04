b59629a769b1371133c4157b255faf6621976587
nivcohen1 <nivcohen1242@gmail.com>
2024-08-04 10:37:37 +0300

    fix small problem

--- Client_Side/src/controllers/LoginController.java
+++ Client_Side/src/controllers/LoginController.java
@@ -15,7 +15,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
